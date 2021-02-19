23d167a36e96ef6302f38a448d20cd9da8c32671
Olcay Sönmez <olcaysnmz@gmail.com>
2021-02-19 23:22:49 +0100

    remove todo

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/VoterInfoResponse.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/VoterInfoResponse.kt
@@ -3,10 +3,6 @@ package com.example.android.politicalpreparedness.network.models
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-class VoterInfoResponse (
-    val election: Election,
-    val state: List<State>? = null,
-    val pollingLocations: String? = null, //TODO: Future Use
-    val contests: String? = null, //TODO: Future Use
-    val electionElectionOfficials: List<ElectionOfficial>? = null
-)
+class VoterInfoResponse(
+        val election: Election,
+        val state: List<State>? = null)
